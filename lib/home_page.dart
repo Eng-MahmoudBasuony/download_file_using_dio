11d45c4b08a51e10546f7ad23be9184d4d431ece
basuony <mbasuony83@gmail.com>
2020-08-05 22:36:17 +0200

    test download image

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -53,8 +53,8 @@ class _HomePageState extends State<HomePage> {
   {
     var dio = Dio();
     String path=await ExtStorage.getExternalStoragePublicDirectory(ExtStorage.DIRECTORY_DOWNLOADS);
-    String fullPath="$path/basuony2.pdf";
+    String fullPath="$path/frymire.png";
 
-    ServiceDownload.download2(dio: dio,url: ApiService.pdfTest1,savePath: fullPath);
+    ServiceDownload.download2(dio: dio,url: ApiService.imageTest2,savePath: fullPath);
   }
 }
--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -2,5 +2,6 @@ class ApiService
 {
    static final String pdfTest1="http://www.orimi.com/pdf-test.pdf";
    static final String pdfTest2 = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
-   static final String imageTest1 = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf";
+   static final String imageTest1 = "https://homepages.cae.wisc.edu/~ece533/images/airplane.png";
+   static final String imageTest2="https://homepages.cae.wisc.edu/~ece533/images/frymire.png";
 }
